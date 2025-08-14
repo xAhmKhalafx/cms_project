e2d30f6dcf82d7497b5259327399247343516263
xAhmKhalafx <ahmed.ashraf.khalaf@gmail.com>
2025-08-14 15:00:44 +1000

    REST API

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Course
+from .serializers import CourseSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def course_list(request):
+    courses = Course.objects.all()
+    serializer = CourseSerializer(courses, many=True)
+    return Response(serializer.data)
