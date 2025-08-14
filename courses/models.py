169661ec4c1e16d2e20579604161c7549ef9562b
xAhmKhalafx <ahmed.ashraf.khalaf@gmail.com>
2025-08-14 14:52:16 +1000

    Adding Model and list of courses

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    lecturer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
